360ced1f9e72388864fe66b6927760bb895026f1
chris lockard <chrislockard21@gmail.com>
2018-09-24 19:47:28 -0400

    hw3.3

--- python_files/homework/hw3/hw3_p5.py
+++ python_files/homework/hw3/hw3_p5.py
@@ -1,5 +1,17 @@
-eol = '\n'
-lines = binil, eol, starly, has, eol, been, eol, teaching, eol,
+from zipfile import *
+from collections import OrderedDict
 
-file = open('writelines.txt', 'w')
-file.writelines(lines)
+zip_archive = ZipFile('5_Jobs_Completed_log.zip', 'r')
+
+sum = 0
+for file in zip_archive.namelist():
+    file_opened = zip_archive.open(file, 'r')
+    lines = file_opened.readlines()
+    total_lines = [
+        line.decode('utf-8').split() for line in lines
+        if line.startswith(b'Jobs Completed..')
+    ]
+    for line in total_lines:
+        sum += int(line[2])
+
+print(sum)
---
+++ python_files/homework/hw3/hw3_p6.py
@@ -0,0 +1,45 @@
+from zipfile import *
+from collections import OrderedDict
+import datetime as dt
+
+def convert_str(string_date):
+    return dt.datetime.strptime(string_date, '%Y-%m-%d')
+
+zip_archive = ZipFile('5_Jobs_Completed_log.zip', 'r')
+
+sum = 0
+max = 0
+max_date = ''
+job_lines = []
+for file in zip_archive.namelist():
+    file_opened = zip_archive.open(file, 'r')
+    lines = file_opened.readlines()
+    total_lines = [
+        line.decode('utf-8').split() for line in lines
+        if line.startswith(b'Jobs Completed..')
+    ]
+
+    for line in total_lines:
+        # Part a
+        if (
+            convert_str(line[3]).date() >= dt.datetime(2018, 8, 15).date() and
+            convert_str(line[3]).date() <= dt.datetime(2018, 9, 15).date()
+        ):
+            sum += int(line[2])
+
+        # Part b
+        if int(line[2]) > max:
+            max = int(line[2])
+            max_date = convert_str(line[3]).date()
+        job_lines.append(line)
+# Part c
+first_date = convert_str(job_lines[0][3]).date()
+last_date = convert_str(job_lines[-1][3]).date()
+
+print('Sum of all jobs completed: {}\n'.format(sum))
+print('Date with the most jobs completed: {}\n'.format(max_date))
+print(
+    'Days between when the first and last job batches completed: {}\n'.format(
+        (last_date - first_date).days
+    )
+)
